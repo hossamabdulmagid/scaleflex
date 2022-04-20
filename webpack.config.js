5734ddcb2e4ab372af65a5c79952efe5daa2ff6c
hossam <hosamabdulmaged@gmail.com>
2022-04-20 08:07:26 +0200

    try to build umd pck

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,54 @@
+const webpack = require("webpack");
 const path = require("path");
+const yargs = require("yargs");
+const env = yargs.argv.env; // use --env with webpack 2
+const pkg = require("./package.json");
+const shouldExportToAMD = yargs.argv.amd;
 
-module.exports = {
-    mode: "development",
-    entry: "./index.js",
+let compareImg = pkg.name;
+
+let outputFile, mode;
+
+if (shouldExportToAMD) {
+    compareImg += ".amd";
+}
+
+if (env === "build") {
+    mode = "production";
+    outputFile = compareImg + ".min.js";
+} else {
+    mode = "development";
+    outputFile = compareImg + ".js";
+}
+
+const config = {
+    mode: mode,
+    entry: __dirname + "/src/index.js",
+    devtool: "source-map",
     output: {
-        path: path.resolve(__dirname, "public"),
-        filename: "main.js",
-    },
-
-    target: "web",
-    devServer: {
-        port: "3000",
-        static: ["./public"],
-        open: true,
-        hot: true,
-        liveReload: true,
-    },
-    resolve: {
-        extensions: [".js", ".jsx", ".json", ".ts"],
+        path: __dirname + "/lib",
+        filename: outputFile,
+        library: compareImg,
+        libraryTarget: shouldExportToAMD ? "amd" : "umd",
+        libraryExport: "default",
+        umdNamedDefine: true,
+        globalObject: "typeof self !== 'undefined' ? self : this",
     },
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: "babel-loader",
+                test: /(\.jsx|\.js|\.ts|\.tsx)$/,
+                use: {
+                    loader: "babel-loader",
+                },
+                exclude: /(node_modules|bower_components)/,
             },
         ],
     },
+    resolve: {
+        modules: [path.resolve("./node_modules"), path.resolve("./src")],
+        extensions: [".json", ".js"],
+    },
 };
+
+module.exports = config;
